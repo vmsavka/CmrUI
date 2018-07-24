0996ee3cda81bcdec6e9b64d5b33e24f903bbf14
Vasyl.Savka <vasyl.savka@appreal.co.il>
2018-07-24 15:16:07 +0300

    add abstraction to dataSource at NewsFeedVM

--- CmrUI/Screens/News Feed tab/NewsFeedVC.swift
+++ CmrUI/Screens/News Feed tab/NewsFeedVC.swift
@@ -16,6 +16,11 @@ fileprivate struct NewsFeedInsets {
     static let kRightEdgeInset: CGFloat = 16
 }
 
+fileprivate enum SegueID: String {
+    case detailedFeedNews = "DetailedFeedNews"
+    case descriptionFeedNews = "DescriptionFeedNews"
+}
+
 private struct Constants {
     
     let kReuseCellID = "FeedNewsCell"
@@ -23,11 +28,6 @@ private struct Constants {
     let kReuseHeader = "FeedNewsSectionHeader"
     let kImgName = "pict_"
     let kImgCellHeight: CGFloat = 0.866
-    
-    enum SegueID: String {
-        case detailedFeedNews = "DetailedFeedNews"
-        case descriptionFeedNews = "DescriptionFeedNews"
-    }
     
     let backgroundColor = UIColor(red: 235.0/255.0, green: 235.0/255.0, blue: 235.0/255.0, alpha: 1.0)
 }
@@ -48,32 +48,24 @@ public struct FeedItem {
     let time: Date? = nil
 }
 
-class NewsFeedVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIViewControllerTransitioningDelegate, UINavigationControllerDelegate {
-    
-    //Will b moved to the model later, it's a stub for now
-    fileprivate var dataSource = [FeedItem]()
+class NewsFeedVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIViewControllerTransitioningDelegate, UINavigationControllerDelegate, NewsFeedVMProtocol {
+
     @IBOutlet weak var collectionView: UICollectionView!
     fileprivate var transitionIndexPath: IndexPath?
     private var customInteractor : CustomInteractor?
     private var selectedFrame : CGRect?
     var zoomTransition: ZoomTransition?
     
+    lazy fileprivate var viewModel: NewsFeedDataSource = {
+        return NewsFeedVM(delegate: self)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupAppearance()
         
-        dataSource = [
-            FeedItem(image: UIImage(named: Constants().kImgName + "\(1)"), title: "First title", subtitle: "First subtitle", content: "First content", feedType: FeedType.feedImage),
-            FeedItem(image: nil, title: "First text title", subtitle: "Only text subtitle", content: "", feedType: FeedType.feedText),
-            FeedItem(image: UIImage(named: Constants().kImgName + "\(2)"), title: "Second title", subtitle: "Second subtitle", content: "Second content", feedType: FeedType.feedImage),
-            FeedItem(image: UIImage(named: Constants().kImgName + "\(3)"), title: "Third title", subtitle: "Third subtitle", content: "The photographs are stunning: a giant mountain of ice towers over a tiny village, with colorful homes reminiscent of little doll houses against the stark, blue-gray landscape.\nBut for the people living in those houses – that beauty could be life-threatening.\nIts kind of like, if you lived in the suburbs, and you woke up one morning and looked out, and there was a skyscraper next to your house,\n says David Holland, an oceanographer at New York University who does research in Greenland during the summer months.I'd be the first to get out of there.\nHe says that's why authorities have taken action to evacuate those living closest to the water from the village of Innaarsuit, where the iceberg has parked itself just off the coast. According to the BBC, the village has just 169 residents.\nIn these shallow bays, these icebergs may drift in and become stuck, grounded on the sea floor, Holland says.So that's what happened to one of these bergs.Holland says it can be quite alarming for residents. Article continues after sponsorship These are small villages with little houses located right at the shoreline, and all of a sudden icebergs show up, and they look like New York skyscrapers, they're just towering, he says.They're very unstable, and they can break up.\nLast year in northwestern Greenland, four people died when a landslide resulted in a tsunami that swamped a number of homes.That disaster is fresh in peoples' minds, says glaciologist Anna Hogg at the University of Leeds, who also does research in Greenland.\nMassive Iceberg Makes A Stop Off Newfoundland Coast World Massive Iceberg Makes A Stop Off Newfoundland Coast There's a risk that a large chunk of ice could break off this very large iceberg, fall into the ocean, and cause a mini-tidal wave that will wash up and hit the village, Dr. Hogg says.\n We know that icebergs are quite fragile things, they've got lots of fractures through them, she adds.\n\nOne surprising thing about many of the seaside communities in Greenland, Hogg says, is that despite the marine-based economy, most people can't swim. Hogg has spent a lot of time doing research near the village that's currently under threat from the iceberg, and says this adds to the risk of a potential tidal wave or flooding.\nThere's only one swimming pool in Greenland. It's in Nuuk, which is much further down the coast than this village that we're talking about, Hogg says. If you think about it, why would they be able to swim? The ocean water is just so cold; you can't even put your toe in without it being unbearably freezing.\nLocal authorities and media are keeping close watch on the iceberg: the newspaper Sermitsiaq reported today that it moved 500-600 meters during the night.\nThough the process of glaciers losing ice is natural, and happens every summer, the waters around Greenland have warmed in recent decades, which means that it's happening at a faster rate.", feedType: FeedType.feedImage),
-            FeedItem(image: UIImage(named: Constants().kImgName + "\(4)"), title: "Fourth title", subtitle: "Fourth subtitle", content: "Fourth content", feedType: FeedType.feedImage),
-            FeedItem(image: nil, title: "A huge iceberg has drifted close to a village in western Greenland, prompting a partial evacuation in case it splits and the resulting wave swamps homes.",
-                     subtitle: "Massive iceberg threatens Greenland village", content: "The photographs are stunning: a giant mountain of ice towers over a tiny village, with colorful homes reminiscent of little doll houses against the stark, blue-gray landscape.\nBut for the people living in those houses – that beauty could be life-threatening.\nIts kind of like, if you lived in the suburbs, and you woke up one morning and looked out, and there was a skyscraper next to your house,\n says David Holland, an oceanographer at New York University who does research in Greenland during the summer months.I'd be the first to get out of there.\nHe says that's why authorities have taken action to evacuate those living closest to the water from the village of Innaarsuit, where the iceberg has parked itself just off the coast. According to the BBC, the village has just 169 residents.\nIn these shallow bays, these icebergs may drift in and become stuck, grounded on the sea floor, Holland says.So that's what happened to one of these bergs.Holland says it can be quite alarming for residents. Article continues after sponsorship These are small villages with little houses located right at the shoreline, and all of a sudden icebergs show up, and they look like New York skyscrapers, they're just towering, he says.They're very unstable, and they can break up.\nLast year in northwestern Greenland, four people died when a landslide resulted in a tsunami that swamped a number of homes.That disaster is fresh in peoples' minds, says glaciologist Anna Hogg at the University of Leeds, who also does research in Greenland.\nMassive Iceberg Makes A Stop Off Newfoundland Coast World Massive Iceberg Makes A Stop Off Newfoundland Coast There's a risk that a large chunk of ice could break off this very large iceberg, fall into the ocean, and cause a mini-tidal wave that will wash up and hit the village, Dr. Hogg says.\n We know that icebergs are quite fragile things, they've got lots of fractures through them, she adds.\n\nOne surprising thing about many of the seaside communities in Greenland, Hogg says, is that despite the marine-based economy, most people can't swim. Hogg has spent a lot of time doing research near the village that's currently under threat from the iceberg, and says this adds to the risk of a potential tidal wave or flooding.\nThere's only one swimming pool in Greenland. It's in Nuuk, which is much further down the coast than this village that we're talking about, Hogg says. If you think about it, why would they be able to swim? The ocean water is just so cold; you can't even put your toe in without it being unbearably freezing.\nLocal authorities and media are keeping close watch on the iceberg: the newspaper Sermitsiaq reported today that it moved 500-600 meters during the night.\nThough the process of glaciers losing ice is natural, and happens every summer, the waters around Greenland have warmed in recent decades, which means that it's happening at a faster rate."
-,
-                feedType: FeedType.feedText),
-                FeedItem(image: UIImage(named: Constants().kImgName + "\(5)"), title: "Fifth title", subtitle: "Fifth subtitle", content: "Fifth content", feedType: FeedType.feedImage)
-        ]
+        viewModel.populateDataSource()
+        
         //self.navigationController?.delegate = self
     }
     
@@ -115,12 +107,15 @@ class NewsFeedVC: UIViewController, UICollectionViewDelegate, UICollectionViewDa
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if type(of: segue.destination) == NewsFeedDetailedVC.self {
             let feedDetailedVC = (segue.destination as! NewsFeedDetailedVC)
-            let item  = dataSource[(transitionIndexPath?.row)!]
+            let item  = viewModel.feedItemForRowAtItemIndex(index: (transitionIndexPath?.row)!)
             feedDetailedVC.feedItem = item
             //feedDetailedVC.transitioningDelegate = self
             feedDetailedVC.modalPresentationStyle = UIModalPresentationStyle.custom
             feedDetailedVC.navigationItem.hidesBackButton = true
         }
+    }
+    
+    func updateDataSource() {
     }
 
     // MARK: UICollectionViewDataSource
@@ -130,16 +125,16 @@ class NewsFeedVC: UIViewController, UICollectionViewDelegate, UICollectionViewDa
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return dataSource.count
+        return viewModel.count
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if dataSource[indexPath.item].feedType == FeedType.feedImage {
+        if viewModel.feedItemForRowAtItemIndex(index: indexPath.item)?.feedType == FeedType.feedImage {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants().kReuseCellID, for: indexPath) as! FeedNewsCell
             
-            cell.backgroundImageView.image = dataSource[indexPath.item].image
-            cell.titleTextView.text = dataSource[indexPath.item].title
-            cell.subtitleLabel.text = dataSource[indexPath.item].subtitle
+            cell.backgroundImageView.image = viewModel.feedItemForRowAtItemIndex(index: indexPath.item)?.image
+            cell.titleTextView.text = viewModel.feedItemForRowAtItemIndex(index: indexPath.item)?.title
+            cell.subtitleLabel.text = viewModel.feedItemForRowAtItemIndex(index: indexPath.item)?.subtitle
             cell.backgroundColor = UIColor.red
             cell.isUserInteractionEnabled = true
             cell.titleTextView.isUserInteractionEnabled = false
@@ -147,8 +142,8 @@ class NewsFeedVC: UIViewController, UICollectionViewDelegate, UICollectionViewDa
         }
         else {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants().kReuseCellTextID, for: indexPath) as! FeedNewsTextCell
-            cell.titleTextView.text = dataSource[indexPath.item].title
-            cell.subtitleLabel.text = dataSource[indexPath.item].subtitle
+            cell.titleTextView.text = viewModel.feedItemForRowAtItemIndex(index: indexPath.item)?.title
+            cell.subtitleLabel.text = viewModel.feedItemForRowAtItemIndex(index: indexPath.item)?.subtitle
             cell.isUserInteractionEnabled = true
             cell.titleTextView.isUserInteractionEnabled = false
             return cell
@@ -175,7 +170,7 @@ class NewsFeedVC: UIViewController, UICollectionViewDelegate, UICollectionViewDa
     func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
         transitionIndexPath = indexPath
         
-        self.performSegue(withIdentifier: "DetailedFeedNews", sender: nil)
+        self.performSegue(withIdentifier: SegueID.detailedFeedNews.rawValue, sender: nil)
         return true
     }
     
@@ -249,8 +244,8 @@ extension NewsFeedVC: CollectionViewFlowLayoutDelegate {
     }
     
     func collectionView(_ collectionView: UICollectionView, heightForItemAt indexPath: IndexPath) -> CGFloat {
-        let item = dataSource[indexPath.item]
-        if item.feedType == FeedType.feedImage {
+        let item = viewModel.feedItemForRowAtItemIndex(index: indexPath.item)
+        if item?.feedType == FeedType.feedImage {
 //            if let height = item.image?.size.height {
 //                return height
 //            }
@@ -258,9 +253,9 @@ extension NewsFeedVC: CollectionViewFlowLayoutDelegate {
                 return Constants().kImgCellHeight * collectionView.frame.size.width
 //            }
         }
-        else if item.feedType == FeedType.feedText {
+        else if item?.feedType == FeedType.feedText {
             let font = UIFont(name: "HelveticaNeue-Bold", size: 20)
-            let size = font?.sizeOfString(string: item.title!, constrainedToWidth: collectionView.frame.size.width - 16)
+            let size = font?.sizeOfString(string: (item?.title!)!, constrainedToWidth: collectionView.frame.size.width - 16)
             
             return (size?.height)! + CGFloat(46)
         }
@@ -304,10 +299,10 @@ extension NewsFeedVC {
     func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationControllerOperation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         let cell = self.collectionView.cellForItem(at: transitionIndexPath!)
         let frame = cell?.frame
-        let item  = dataSource[(transitionIndexPath?.row)!]
+        let item  = viewModel.feedItemForRowAtItemIndex(index: (transitionIndexPath?.row)!)
         var img: UIImage = UIImage()
-        if item.image != nil {
-            img = item.image!
+        if item?.image != nil {
+            img = (item?.image!)!
         }
         
         switch operation {
@@ -322,10 +317,10 @@ extension NewsFeedVC {
     func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         
         guard let frame = self.selectedFrame else { return nil }
-        let item  = dataSource[(transitionIndexPath?.row)!]
+        let item  = viewModel.feedItemForRowAtItemIndex(index: (transitionIndexPath?.row)!)
         var img: UIImage = UIImage()
-        if item.image != nil {
-            img = item.image!
+        if item?.image != nil {
+            img = (item?.image!)!
         }
         
         self.zoomTransition = ZoomTransition(duration: TimeInterval(UINavigationControllerHideShowBarDuration), isPresenting: true, originFrame: frame, image: img)
